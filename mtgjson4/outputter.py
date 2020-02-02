295ab07c47536c62d5afe750cef5de8313aff288
ZeldaZach <zahalpern+github@gmail.com>
2020-02-01 22:03:34 -0500

    ADD isOnline/isPaper to SetList
    Signed-off-by: ZeldaZach <zahalpern+github@gmail.com>

--- mtgjson4/outputter.py
+++ mtgjson4/outputter.py
@@ -247,6 +247,8 @@ def get_all_set_list(files_to_ignore: List[str]) -> List[Dict[str, str]]:
                 "code": file_content.get("code"),
                 "meta": file_content.get("meta"),
                 "name": file_content.get("name"),
+                "isOnlineOnly": file_content.get("isOnlineOnly", False),
+                "isPaperOnly": file_content.get("isPaperOnly", False),
                 "releaseDate": file_content.get("releaseDate"),
                 "totalSetSize": file_content.get("totalSetSize"),
                 "type": file_content.get("type"),
